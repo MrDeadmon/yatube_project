f036964dbc366cb4e568b530939d614fa1e021e5
Эдмон Айвазян <mr.deadmon@gmail.com>
2022-12-05 14:53:06 +0500

    проврека 05.12

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,16 +3,39 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Post, Group
 
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
-
-
-# Страница со списком мороженого
-def posts_list(request):
-    return HttpResponse('Список постов')
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'посты, отфильтрованные по группам.{slug}')
+    """Посты по группам"""
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def group_list(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': f'Все сообщения группы {group.title}',
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
